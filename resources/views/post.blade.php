af775d3a779c6a25428c03f52d2d61f2fdbfe201
Abizar Valentino Fachri <203040086@main.unpas.ac.id>
2022-10-28 15:33:05 +0700

    Mempelajari Post Category dan Eloquent Relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,10 +1,12 @@
 @extends('layouts.main')
 
 @section('container')
-  <h2>{{ $post->title }}</h2>
+  <h1 class="mb-5">{{ $post->title }}</h1>
+
+  <p>By. Abizar Valentino Fachri in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
 
 {!! $post->body !!}
- 
+
 
   <a href="/blog">Back to Posts</a>
 
