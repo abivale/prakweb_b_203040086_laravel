b0539cd1fd2d7e8dc324440f289284b3c0ae03b6
Abizar Valentino Fachri <203040086@main.unpas.ac.id>
2022-11-24 21:03:34 +0700

    Mempelajari User Registration

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -8,7 +8,7 @@ class LoginController extends Controller
 {
     public function index()
     {
-        return view('login/index', [
+        return view('login.index', [
             'title' => 'Login',
             'active' => 'login'
         ]);
