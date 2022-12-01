7ea713d4467c43979005157a6a9f511cf6867a1c
Abizar Valentino Fachri <203040086@main.unpas.ac.id>
2022-12-01 17:48:45 +0700

    Mempelajari Create Post Form

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory,
+        Sluggable;
 
     // protected $fillable = ['title', 'excerpt', 'body'];
     protected $guarded = ['id'];
@@ -50,5 +53,13 @@ class Post extends Model
         return 'slug';
     }
 
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
 
 }
